6b0eb913712130ff1b84c1b509460307d94c0809
ahan <ahankamat45@gmail.com>
2022-09-29 16:54:25 +0530

    code added

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+class Multiply
+{
+ public static void main(String args[])
+ {
+   int a=3,b=5;
+   int c=a*b;
+   System.out.println("pdt is"+c);
+}
+}
---
+++ Sum.java
@@ -0,0 +1,10 @@
+
+class Sum
+{
+ public static void main(String args[])
+ {
+   int a=4,b=7;
+   int c=a+b;
+   System.out.println("sum is"+c);
+}
+}
